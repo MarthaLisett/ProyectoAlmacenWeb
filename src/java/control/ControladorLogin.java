1f6be71dac5a2a70c45c88c5e6394eb049f5373f
Jose Gonzalez <josegzza@gmail.com>
2017-01-10 15:43:17 -0600

    Quita prints

--- src/java/control/ControladorLogin.java
+++ src/java/control/ControladorLogin.java
@@ -47,8 +47,8 @@ public class ControladorLogin extends HttpServlet {
             SQLException {
         // pasar matricula a letras minusculas para hacer la busqueda
         matricula = request.getParameter("matricula").toLowerCase();
-        System.out.println("EXISTE:" + ManejoBasesDatos.existe(usuario));
         usuario   = new Usuario(matricula);
+
         // revisar si el usuario existe
         if (ManejoBasesDatos.existe(usuario)){
             url = urlTipoUsuario(usuario);
